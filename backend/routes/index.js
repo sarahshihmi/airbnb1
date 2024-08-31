023d09395df94549b8dd287640ed6c71b69a443e
Sarah <sarahashih@gmail.com>
2024-08-31 14:24:56 -0700

    done with phase 1 added api server

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
@@ -9,5 +13,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
