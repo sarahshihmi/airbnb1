2b9e8c22f52f9b93f068652f38c2de2bf512ba1e
Sarah <sarahashih@gmail.com>
2024-09-24 14:06:28 -0700

    added put and delete for reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,7 +7,7 @@ const { Review, ReviewImage, User, Spot } = require('../../db/models');
 
 router.use(handleValidationErrors)
 
-router.get('/current', requireAuth, async(req, res, next)=> {
+router.get('/current', requireAuth, async(req, res)=> {
     const userId = req.user.id
     const reviews = await Review.findAll({
         where:{
@@ -29,7 +29,7 @@ router.get('/current', requireAuth, async(req, res, next)=> {
     res.json(reviews)
 })
 
-router.post('/:reviewId/images', requireAuth, async(res, req, next) => {
+router.post('/:reviewId/images', requireAuth, async(res, req) => {
         const {reviewId} = req.params;
         const userId = req.user.id
         const review = await Review.findOne({
@@ -46,7 +46,56 @@ router.post('/:reviewId/images', requireAuth, async(res, req, next) => {
                 message: 'Forbidden'
             })
         }
+
+        if (!review) {
+            return res.status(404).json({
+              message: "Review couldn't be found",
+            });
+          }
         const newImage = await review.createReviewImage(req.body);
         res.status(201).json(newImage);
+})
+
+
+router.put('/:reviewId', requireAuth, async(res, req) => {
+    const { reviewId } = req.params;
+    const userId = req.user.id;
+    const review = await Review.findByPk(reviewId);
+
+    if (review.userId !== userId) {
+        return res.status(403).json({
+          message: 'Forbidden',
+        });
+    }
+    if (!review) {
+        return res.status(404).json({
+          message: "Review couldn't be found",
+        });
+    }
+    await review.update(req.body);
+    await review.save();
+  
+    res.json(review);
+  
+router.delete('/:reviewId', requireAuth, async(res, req)=> {
+    const { reviewId } = req.params;
+    const userId = req.user.id;
+    const review = await Review.findByPk(reviewId);
+
+    if (review.userId !== userId) {
+        return res.status(403).json({
+          message: 'Forbidden',
+        });
+    }
+    if (!review) {
+        return res.status(404).json({
+          message: "Review couldn't be found",
+        });
+    }
+    await review.destroy()
+    res.json({
+        message: "Sucessfully deleted"
+    })
+})
 })
 module.exports = router;
